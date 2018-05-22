9571b9d885aaf15ce58ddaa83c490dd7f19112dd
ubergesundheit <ubergesundheit@googlemail.com>
2018-05-22 21:15:45 +0200

    error on missing arguments

--- confighelper.js
+++ confighelper.js
@@ -1,3 +1,10 @@
 'use strict';
 
-module.exports = require('minimist')(process.argv.slice(2));
+const args = require('minimist')(process.argv.slice(2));
+
+if (!args['elasticsearch-url'] || !args['wfs-source-url']) {
+  console.error('Missing arguments --elasticsearch-url or --wfs-source-url');
+  process.exit(1);
+}
+
+module.exports = args
